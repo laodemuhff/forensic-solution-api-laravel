d429102dea64b66eaa14089cf7cdd82b91671f5d
HnRizaL <hanifrizal@outlook.com>
2020-02-09 06:58:32 +0700

    update dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Aturan;
+use App\Char;
+use App\App;
+use App\Fung;
+use App\History;
 
 class DashboardController extends Controller
 {
@@ -12,7 +17,7 @@ class DashboardController extends Controller
      * @return void
      */
     public function __construct()
-    {
+    {   
         $this->middleware('auth');
     }
 
@@ -23,6 +28,11 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        return view('admin.dashboard');
+        $apps = App::all()->count();
+        $aturans = Aturan::all()->count();
+        $fungs = Fung::all()->count();
+        $chars = Char::all()->count();
+        $histories = history::orderBy('created_at', 'DESC')->take(5)->get();
+        return view('admin.dashboard')->withChars($chars)->withFungs($fungs)->withAturans($aturans)->withApps($apps)->withHistories($histories);
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,30 +3,168 @@
 @section('title', "| Dashboard")
 
 @section('content')
+
+@if (Auth::user()->admin == 1)
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                @if (Auth::user()->admin == 1)
-                <div class="card-header">Dashboard Admin</div>
-                @else
-                <div class="card-header">Dashboard User</div>
-                @endif
+        <div class="col">
+            <div class="card bg-success text-white shadow">
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    @if (Auth::user()->admin == 1)
                     {{Auth::user()->name}}, Anda login sebagai Admin.
-                    @else
-                    {{Auth::user()->name}}, Anda login sebagai User.
-                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Tool</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $apps }}</div>
+                        </div>
+
+                        <div class="col-auto">
+                            <a href="/admin/tools"><i class="fas fa-chevron-right fa-2x"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-success shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Fungtionality</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $fungs }}</div>
+                        </div>
+
+                        <div class="col-auto">
+                            <a href="/admin/funcs"><i class="fas fa-chevron-right fa-2x text-success"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-info shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Characteristics</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $chars }}</div>
+                        </div>
+
+                        <div class="col-auto">
+                            <a href="/admin/chars"><i class="fas fa-chevron-right fa-2x text-info"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-warning shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Rule</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $aturans }}</div>
+                        </div>
+
+                        <div class="col-auto">
+                            <a href="/admin/rules"><i class="fas fa-chevron-right fa-2x text-warning"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-xl-12 col-lg-11">
+            <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-primary">Latest Histories</h6>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-bordered" width="100%" cellspacing="0">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>User</th>
+                                    <th>Rule</th>
+                                    <th>Created_at</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                @foreach ($histories as $history)
+                                <tr>
+                                    <th>{{ $history->id }}</th>
+                                    <td>{{ $history->id_user }}</td>
+                                    <td>{{ $history->id_aturan }}</td>
+                                    <td>{{ $history->created_at }}</td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+@else
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col">
+            <div class="card bg-success text-white shadow">
+
+                <div class="card-body">
+                    {{Auth::user()->name}}, Anda login sebagai User.
+                </div>
+            </div>
+        </div>
+    </div><br/>
+    <div class="row">
+        <div class="col">
+            <div class="card text-center">
+                <div class="card-body text-center">
+                <h2 class="card-title text-primary">Alur Proses Sistem</h2>
+                    <div class="row no-gutters align-items-center text-center">
+                        <div class="col-md-2 bg-primary text-white shadow p-2 rounded">
+                            <div class="mb-1 text-center">Memilih Fungsionalitas dan Karakteristik</div>
+                        </div>
+                        <div class="col text-center"><i class="text-primary fas fa-chevron-right fa-2x"></i>
+                        </div>
+
+                        <div class="col-md-2 bg-primary text-white shadow p-2 rounded">
+                            <div class="mb-1 text-center"> Melakukan Pengecekan Data</div>
+                        </div>
+                        <div class="col text-center"><i class="text-primary fas fa-chevron-right fa-2x"></i>
+                        </div>
+
+                        <div class="col-md-2 bg-primary text-white shadow p-2 rounded">
+                            <div class="mb-1 text-center"> Menampilkan Hasil Aplikasi</div>
+                        </div>
+                        <div class="col text-center"><i class="text-primary fas fa-chevron-right fa-2x"></i>
+                        </div>
+
+                        <div class="col-md-2 bg-primary text-white shadow p-2 rounded">
+                            <div class="mb-1 text-center"> Menyimpan Hasil Aplikasi</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endif
 @endsection
